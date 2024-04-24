3d0a760480c9fc4d42a500af6cc6cc371648a2bf
迷你强 <chenglei1234@gmail.com>
2024-04-24 11:59:40 +0800

    添加陀螺仪积分得偏航数据

--- examples/attitude/attitude.ino
+++ examples/attitude/attitude.ino
@@ -1,7 +1,9 @@
 #include "mpu6887.h"
 #include <math.h>
-
 mpu6887 mpu;
+
+float yaw = 0.0; // 偏航角初始化为0
+unsigned long lastTime = 0; // 上一次更新时间
 void setup()
 {
   mpu.begin();
@@ -15,6 +17,12 @@ void loop()
   int acc_y = mpu.ACCEL_Y();
   int acc_z = mpu.ACCEL_Z();
 
+  int gyro_z = mpu.GYRO_Z_dps(); // 获取陀螺仪Z轴的度每秒值
+  unsigned long currentTime = millis();
+  float deltaTime = (currentTime - lastTime) / 1000.0; // 时间差，单位为秒
+
+  yaw += gyro_z * deltaTime; // 累加计算偏航角
+  lastTime = currentTime; // 更新时间戳
   float ax = acc_x / 32768.0 * 2; // 将加速度计数据规范化到+-2G
   float ay = acc_y / 32768.0 * 2;
   float az = acc_z / 32768.0 * 2;
@@ -22,7 +30,14 @@ void loop()
   float pitch = atan2(-ay, az) * (180.0 / M_PI);
   float roll = atan2(ax, sqrt(ay * ay + az * az)) * (180.0 / M_PI);
 
+  if (yaw > 360) {
+    yaw -= 360;
+  } else if (yaw < 0) {
+    yaw += 360;
+  }
   Serial.print(pitch);  //俯仰 绕X轴旋转,单位度
   Serial.print(",");
-  Serial.println(roll);   //横滚 绕Y轴旋转 单位度
+  Serial.print(roll);   //横滚 绕Y轴旋转 单位度
+  Serial.print(",");
+  Serial.println(yaw);  //偏航 绕Z轴旋转 单位度
 }
